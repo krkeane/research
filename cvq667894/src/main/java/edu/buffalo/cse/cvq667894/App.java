3e2a24c8b624376eb02fef1942bebc71e6176eb9
Kevin R Keane <krkeane@qpm.cc>
2025-09-05 12:14:26 -0400

    Initial notes.

---
+++ cvq667894/src/main/java/edu/buffalo/cse/cvq667894/App.java
@@ -0,0 +1,10 @@
+package edu.buffalo.cse.cvq667894;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
